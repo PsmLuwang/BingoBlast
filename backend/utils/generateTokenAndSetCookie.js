70f3091558390e18995ce3055d856f2fc9664f8a
Priyansu <spriyansumeitei@gmail.com>
2025-08-01 15:03:49 +0530

    set cookies for safari

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -1,12 +1,15 @@
 import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (res, userId) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET);
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "30d", // optional: matches cookie
+	});
 
 	res.cookie("token", token, {
 		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
+		secure: true,
 		sameSite: "None",
+		maxAge: 30 * 24 * 60 * 60 * 1000, // 1 month
 	});
 
 	return token;
